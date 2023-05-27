a6c4915e9e263b37a16edcb456faff74527a2462
Olof Bjarnason <olof.bjarnason@gmail.com>
2023-05-27 17:34:28 +0200

    Test for vertical move

--- test/Main.purs
+++ test/Main.purs
@@ -4,16 +4,21 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Aff (launchAff_)
-import Test.Spec.Runner (runSpec)
-import Test.Spec.Reporter.TeamCity (teamcityReporter)
-import Test.Spec (describe, it)
-import Test.Spec.Assertions (shouldEqual)
 import Main (tick)
+import Test.Spec (describe, it, pending')
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.TeamCity (teamcityReporter)
+import Test.Spec.Runner (runSpec)
+import Data.Number.Format (fixed, toStringWith)
+
+
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [teamcityReporter] $
     describe "Germ behaviour" do
-        it "should be able to move" do
+        it "can convert numbers to strings" do
+            toStringWith (fixed 1) 0.12 # shouldEqual "0.1"
+        it "should be able to move horisontally" do
             tick {
                 germs : [{pos: { x:0.0, y:0.0}, dir: 0.0, age: 0}],
                 foods: []
@@ -21,7 +26,13 @@ main = launchAff_ $ runSpec [teamcityReporter] $
                 germs : [{pos: { x:1.0, y:0.0}, dir: 0.0, age: 1}],
                 foods: []
                 }
--- moving germ
+        it "should be able to move verticalliy" do
+            let nextState = tick {
+                germs : [{pos: { x:0.0, y:0.0}, dir: 3.1415/2.0, age: 0}],
+                foods: []
+            }
+            nextState.germs <#> _.pos.y <#> toStringWith (fixed 1) # shouldEqual ["1.0"]
+-- moving germß
 -- - ordinary move
 -- - eating food
 -- - onto wall
