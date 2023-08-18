d8f6e3b2b4efefecd4569c14613074c89ffa2ee0
Olof Bjarnason <olof.bjarnason@gmail.com>
2023-08-18 04:02:01 +0200

    move meals to separate file

---
+++ src/Meals.purs
@@ -0,0 +1,26 @@
+module Meals where
+
+import MealTypes (Meals)
+
+standardMatsedel :: Meals
+standardMatsedel =
+  [ { meal: "Stekt lax med rotfrukter i ugn"
+    , ingredients:
+        [ { name: "Laxfilé", amount: 1.0, unit: "st" }
+        , { name: "Fast potatis", amount: 1.0, unit: "st" }
+        , { name: "Morot", amount: 1.0, unit: "st" }
+        , { name: "Sötpotatis", amount: 1.0, unit: "st" }
+        , { name: "Rödlök", amount: 0.¡¡5, unit: "st" }
+        , { name: "Vitlök", amount: 2.25, unit: "st" }
+        , { name: "Olivolja", amount: 0.5, unit: "msk" }
+        , { name: "Smör", amount: 1.0, unit: "msk" }
+        , { name: "Citronpeppar", amount: 0.0, unit: "-" }
+        , { name: "Yoghurt", amount: 0.4, unit: "dl" }
+        , { name: "Fetaost", amount: 40.0, unit: "g" }
+        , { name: "Örter", amount: 0.25, unit: "dl" }
+        ]
+    , servings: 2
+    , webPage: "https://www.mathem.se/recept/lax-i-ugn-med-rotfrukter-och-fetaost"
+    }
+  ]
+
